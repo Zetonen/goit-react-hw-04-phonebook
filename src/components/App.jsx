569723d8b3ffdfa40e1142e9fc01c778c80331a5
Seven <graf.lancelot7@gmail.com>
2023-09-20 21:16:40 +0300

    update proj (03 )

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { getContacts, addContacts } from 'services/contact-api';
 import { Section, Title, SubTitle } from './App.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = getContacts();
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+  componentDidUpdate = (prevProps, prevState) => {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      addContacts(contacts);
+    }
   };
 
   addContact = contact => {
@@ -23,11 +32,9 @@ export class App extends Component {
       alert(`${contact.name} is already in contacts`);
       return;
     }
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, { ...contact, id: nanoid() }],
-      };
-    });
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...contact, id: nanoid() }],
+    }));
   };
 
   onChangeFilter = ({ target }) => {
@@ -45,8 +52,10 @@ export class App extends Component {
 
   onDeleteContact = contactId => {
     this.setState(prevState => {
+      const contacts = prevState.contacts.filter(({ id }) => id !== contactId);
+      addContacts(contacts);
       return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+        contacts,
       };
     });
   };
